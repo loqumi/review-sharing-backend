1ba4c614855923fb47b8d96289d9b6963a35c3dd
loqumi <loqumihh@gmail.com>
2023-01-04 04:25:35 +0500

    google oauth change

--- passport.js
+++ passport.js
@@ -6,19 +6,20 @@ import { LoginByOAuth, createUserByOAuth } from "./controllers/Auth.js";
 import passport from "passport";
 
 const GOOGLE_CLIENT_ID =
-  "462098508068-qchslj5nda01d1hm058mg9vohjb7t1a4.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
+  "462098508068-5vda6uetatj6ukfan5clqfi8dufovhno.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
 const GITHUB_CLIENT_ID = "9043ced65582926a6326";
 const GITHUB_CLIENT_SECRET = "55900c170a4ae20e43a076eb9a92fb70521f52f1";
 const DISCORD_CLIENT_ID = "1052566896014282782";
 const DISCORD_CLIENT_SECRET = "9gVciSZyb5fHpxa1p8WbRQESXOH7UVby";
+const URL = "https://webapp-backend-production.up.railway.app";
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "/google/callback",
+      callbackURL: `${URL}/google/callback`,
     },
     async function (accessToken, refreshToken, profile, done) {
       done(null, profile);
